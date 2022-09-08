88001f2f8b04c06c9fb1e023f1a21366196680bf
Mark Leager <106631893+mleager@users.noreply.github.com>
2022-09-08 18:25:21 -0400

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 import pytest
 
-# Create your tests here.
 
 # Create a fixture function that will create a test user
 @pytest.fixture
